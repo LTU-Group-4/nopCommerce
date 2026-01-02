a729acd7cd347adcf14b0454d6eaf4f1d96a31af
Nattarintra Thutsanti <nattarintra.thutsanti@hotmail.com>
2026-01-02 17:46:31 +0100

    Add rester di in plugin

---
+++ src/Plugins/Nop.Plugin.Misc.Polls/Infrastructure/PollsStartup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Infrastructure; // why no use here? why we need it?
+using Nop.Plugin.Misc.Polls.Services;
+
+namespace Nop.Plugin.Misc.Polls.Infrastructure
+{
+    public class PollsStartup : IStartup
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IPollService, PollService>();
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+            
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Order => 1000;
+    }
+}
