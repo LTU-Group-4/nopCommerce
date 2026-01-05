965d395568f44ae84ff0396b1fd82c80b3abee50
Nattarintra Thutsanti <nattarintra.thutsanti@hotmail.com>
2026-01-05 11:33:35 +0100

    Refactored: changed INopStartup

--- src/Plugins/Nop.Plugin.Misc.Polls/Infrastructure/PollsStartup.cs
+++ src/Plugins/Nop.Plugin.Misc.Polls/Infrastructure/PollsStartup.cs
@@ -1,29 +1,37 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Nop.Core.Infrastructure; 
+using Nop.Core.Infrastructure;
 using Nop.Plugin.Misc.Polls.Services;
 
-namespace Nop.Plugin.Misc.Polls.Infrastructure
+namespace Nop.Plugin.Misc.Polls.Infrastructure;
+
+/// <summary>
+/// Represents object for configuring Poll plugin services on application startup
+/// </summary>
+public class PollsStartup : INopStartup
 {
-    public class PollsStartup : IStartup
+    /// <summary>
+    /// Add and configure services for the Poll plugin
+    /// </summary>
+    /// <param name="services">Collection of service descriptors</param>
+    /// <param name="configuration">Configuration of the application</param>
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddScoped<IPollService, PollService>();
-        }
-
-        public void Configure(IApplicationBuilder application)
-        {
-            
-        }
-
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Order => 1000;
+        services.AddScoped<IPollService, PollService>();
     }
+
+    /// <summary>
+    /// Configure the using of added middleware
+    /// </summary>
+    /// <param name="application">Builder for configuring an application's request pipeline</param>
+    public void Configure(IApplicationBuilder application)
+    {
+        
+    }
+
+    /// <summary>
+    /// Gets order of this startup configuration implementation
+    /// </summary>
+    public int Order => 3000;
 }
